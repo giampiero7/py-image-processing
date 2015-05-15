ae398ace56da881a11c11dfa4c34ff7ca24b8832
Giampiero <giampiero.baggiani@gmail.com>
2015-05-15 16:03:44 +0200

    new transparent-bg.py

---
+++ transparent-bg.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import Image
+
+if len(sys.argv) < 2:
+	print ''
+	print '\tUsage: python ' + sys.argv[0] + ' image_file'
+	print '\tExample: python ' + sys.argv[0] + ' img.png'
+	print ''
+	sys.exit(0)
+
+def transpare(image):
+    return image.point(lambda p: p * 0)
+
+for i in range(1, len(sys.argv)):
+	img = Image.open(sys.argv[i]).convert('RGBA')
+	datas = img.getdata()
+	newData = []
+	for item in datas:
+	    if item[0] == 255 and item[1] == 255 and item[2] == 255:
+	        newData.append((255, 255, 255, 0))
+	    else:
+	        newData.append(item)
+	img.putdata(newData)
+	img.save(os.path.splitext(sys.argv[i])[0] + '_transp' + os.path.splitext(sys.argv[i])[1])
